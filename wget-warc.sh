1e312afeffa95df5ccf07f10d20f2532d29e42f7
Rasmus Kriest <rasmusk@outlook.com>
2017-06-21 10:08:03 +0200

    Change wait to 5

--- wget-warc.sh
+++ wget-warc.sh
@@ -13,6 +13,6 @@ wget $url \
     --span-hosts \
     --domains=$url \
     --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36"\
-    --wait=10 \
+    --wait=5 \
     --random-wait \
     --append-output="$1".log
