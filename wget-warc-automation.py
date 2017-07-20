b9f90ea7bc13b2f6b2980bc747d2748255ccb07c
Rasmus Kriest <rasmusk@outlook.com>
2017-07-20 12:22:01 +0200

    Append state after finished download

--- wget-warc-automation.py
+++ wget-warc-automation.py
@@ -3,11 +3,24 @@
 import csv
 import subprocess
 
-# Define webarchivelist to be the provided CSV with websites
-webarchivelist = './example.csv'
+def archive_website(webarchivelist):
+    with open(webarchivelist, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=csvdelimiter)
+        for row in reader:
+            subprocess.run(['./wget-warc.sh', row['URL']])
 
-with open(webarchivelist) as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        subprocess.run(['./wget-warc.sh', row['URL']])
-        
+def append_csv(webarchivelist):
+    with open(webarchivelist, 'a') as csvfile:
+        writer = csv.writer(csvfile, delimiter=csvdelimiter)
+        writer.writerow({'done'})
+
+
+if __name__ == "__main__":
+    # Define webarchivelist to be the provided CSV with websites
+    webarchivelist = './example.csv'
+    csvheader = ['Organization','URL','Info1','Info2','Info3','Last','State']
+    csvdelimiter = ','
+    
+    archive_website(webarchivelist)
+    append_csv(webarchivelist)
+                    
