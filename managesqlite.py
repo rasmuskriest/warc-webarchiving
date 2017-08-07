14f5799c0dd52b062185fd9407887fa0bf8dd22c
Rasmus Kriest <rasmusk@outlook.com>
2017-08-07 14:33:56 +0200

    Create module to work with SQLite

---
+++ managesqlite.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+def create_sqlite():
+    """Create SQLite database."""
+
+def import_csv():
+    """Import CSV to SQLite database."""
+
+def clear_sqlite():
+    """Clear SQLite database."""     
