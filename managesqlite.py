37717f827fa0b5045a38d89da8c06145309a5336
Rasmus Kriest <rasmusk@outlook.com>
2017-08-07 15:17:43 +0200

    Add functions for checking and export

--- managesqlite.py
+++ managesqlite.py
@@ -3,11 +3,17 @@
 
 import sqlite3
 
+def check_sqlite():
+    """Check for SQLite database."""
+
 def create_sqlite():
     """Create SQLite database."""
 
 def import_csv():
-    """Import CSV to SQLite database."""
+    """Import CSV file to SQLite database."""
 
 def clear_sqlite():
-    """Clear SQLite database."""     
+    """Clear SQLite database."""
+
+def export_csv():
+    """Export SQLite database to CSV file."""
