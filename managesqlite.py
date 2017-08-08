cbd5a48bb56ea96a0335311f5794b4e7ecb19600
Rasmus Kriest <rasmusk@outlook.com>
2017-08-08 11:54:29 +0200

    Move clear_sqlite() to top, remove create_sqlite()

--- managesqlite.py
+++ managesqlite.py
@@ -3,14 +3,11 @@
 
 import sqlite3
 
-def create_sqlite():
-    """Create SQLite database."""
-
-def import_csv():
-    """Import CSV file to SQLite database."""
-
 def clear_sqlite():
     """Clear SQLite database."""
 
-def export_csv():
+def import_csv(csvfile, dbfilename):
+    """Import CSV file to SQLite database."""
+
+def export_csv(csvfile, dbfilename):
     """Export SQLite database to CSV file."""
