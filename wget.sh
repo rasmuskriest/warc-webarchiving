d588bc64dacc3aa36dc6f3ebb6c932b86b281740
Rasmus Kriest <rasmusk@outlook.com>
2017-09-25 12:13:22 +0200

    Reduce wait

--- wget.sh
+++ wget.sh
@@ -16,6 +16,6 @@ wget $url \
     --span-hosts \
     --domains=$url \
     --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36"\
-    --wait=5 \
+    --wait=1 \
     --random-wait \
     --append-output="$dir/$url".log
