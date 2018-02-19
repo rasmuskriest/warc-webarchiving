338248cfa777463f337ccb509c501f94db1475d4
Rasmus Kriest <rasmusk@outlook.com>
2018-02-19 14:55:43 +0100

    Download with seperate function, fix some errors after variable renaming

--- archivesites.py
+++ archivesites.py
@@ -31,16 +31,25 @@ def check_time():
         return False
 
 
-def write_state(conn, elem, sheet_name, column_url, column_last, column_state):
+def write_state(conn, elem, import_sheet):
     """Write new state in respecting column of the database."""
     writecurs = conn.cursor()
 
     writecurs.execute('UPDATE {tn} SET {cl}=(?), {cs}=("done") WHERE {idf}=(?)'.
-                      format(tn=sheet_name, cl=column_last, cs=column_state, idf=column_url), (date.today(), elem))
-    logging.info("%s successfully marked as done in %s", elem, sheet_name)
+                      format(tn=import_sheet, cl="Last", cs="State", idf="Url"), (date.today(), elem))
+    logging.info("%s successfully marked as done in %s", elem, import_sheet)
 
 
-def archive_websites(download_dir, database, sheet_name, column_url, column_last, column_state):
+def download_site(download_dir, import_sheet, conn, elem):
+    """Actually download the site with a subprocess."""
+    logging.info("Downloading %s with subprocess.run() at %s",
+                 elem, datetime.now())
+    subprocess.run(['./wget.sh', elem, download_dir])
+    write_state(conn, elem, import_sheet)
+    logging.info("%s successfully downloaded with subprocess.run()", elem)
+
+
+def archive_websites(download_dir, database, import_sheet, column_names):
     """Archive websites from a csv"""
     sqlite_exists = managesqlite.check_sqlite(database)
     download_time = check_time()
@@ -55,16 +64,11 @@ def archive_websites(download_dir, database, sheet_name, column_url, column_last
             logging.info("download_time is True")
             # Select URL in rows that are not done
             readcurs.execute('SELECT ({coi}) FROM {tn} WHERE {cn} IS NULL'.
-                             format(coi=column_url, tn=sheet_name, cn=column_state))
+                             format(coi="Url", tn=import_sheet, cn="State"))
             for row in readcurs:
                 logging.info(readcurs)
                 for elem in row:
-                    logging.info("Downloading %s with subprocess.run()", elem)
-                    subprocess.run(['./wget.sh', elem, download_dir])
-                    write_state(conn, elem, sheet_name, column_url,
-                                column_last, column_state)
-                    logging.info(
-                        "%s successfully downloaded with subprocess.run()", elem)
+                    download_site(download_dir, import_sheet, conn, elem)
             conn.commit()
             conn.close()
 
