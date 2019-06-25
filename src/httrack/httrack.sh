734c769578692c43842b0bafe14a9e895b29a0a1
rasmuskriest <rasmusk@outlook.com>
2019-06-25 15:06:31 +0200

    Fix typo

--- src/httrack/httrack.sh
+++ src/httrack/httrack.sh
@@ -13,7 +13,7 @@ httrack $url \
     -O "$dir/$folder" \
     --assume php3=text/html,php=text/html,php4=text/html,php2=text/html,cgi=text/html,asp=text/html,jsp=text/html,pl=text/html,cfm=text/html \
     -ad.doubleclick.net/* -mime:application/foobar \
-    +*.png +*.gif +*.jpg +*.jpeg +*.css +*.js -ad.doubleclick.net/* -mime:application/foobar +mime:text/html +mime:image/*
+    +*.png +*.gif +*.jpg +*.jpeg +*.css +*.js -ad.doubleclick.net/* -mime:application/foobar +mime:text/html +mime:image/* \
     -F "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36" \
     -d \
     --search-index \
