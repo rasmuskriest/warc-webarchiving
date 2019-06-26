942f2dcf635746344e28f5a48dcf4f2d0f5c55b5
rasmuskriest <rasmusk@outlook.com>
2019-06-26 08:41:00 +0200

    Update HTTrack script

--- src/httrack/httrack.sh
+++ src/httrack/httrack.sh
@@ -7,16 +7,15 @@ dir=$3
 mkdir -p $dir
 
 httrack $url \
-    --mirrorlinks \
+    --mirror \
     --quiet \
     --debug-headers \
-    -O "$dir/$folder/httrack" \
+    -O $folder \
     --assume php3=text/html,php=text/html,php4=text/html,php2=text/html,cgi=text/html,asp=text/html,jsp=text/html,pl=text/html,cfm=text/html \
-    -ad.doubleclick.net/* -mime:application/foobar \
-    +*.png +*.gif +*.jpg +*.jpeg +*.css +*.js -ad.doubleclick.net/* -mime:application/foobar +mime:text/html +mime:image/* \
     -F "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36" \
     -a \
     --search-index \
     -s2 \
     -u2 \
-    --max-rate=10000000
+    --max-rate=10000000 \
+    -ad.doubleclick.net/* -mime:application/foobar +*.png +*.gif +*.jpg +*.jpeg +*.css +*.js +mime:text/html +mime:image/*
