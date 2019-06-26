50f004532264bea2605dbfb1b2bdfad7013d062e
rasmuskriest <rasmusk@outlook.com>
2019-06-26 09:11:15 +0200

    Put folder variable in quotes

--- src/httrack/httrack.sh
+++ src/httrack/httrack.sh
@@ -10,7 +10,7 @@ httrack $url \
     --mirror \
     --quiet \
     --debug-headers \
-    -O $folder \
+    -O "$folder" \
     --assume php3=text/html,php=text/html,php4=text/html,php2=text/html,cgi=text/html,asp=text/html,jsp=text/html,pl=text/html,cfm=text/html \
     -F "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36" \
     -a \
